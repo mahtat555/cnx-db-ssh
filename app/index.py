3dab1d7f1c55c1be5f1b65b6a2af0f2de9693c5b
mahtat555 <yassinmhtat@gmail.com>
2021-09-26 00:31:10 +0100

    Add home page to our application

--- app/index.py
+++ app/index.py
@@ -6,17 +6,21 @@ it is the default page or home page.
 
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     """ This function return the home page
     """
-    return "Hello"
+    databases = {
+        "database 1": ["table 1", "table 2"],
+        "database 2": ["table 3", "table 4", "table 5", "table 6"],
+    }
+    return render_template("home.jinja", databases=databases)
 
 
 def main():
